aee5e48bd9f78d72709ddd666964584f2cb5b24e
MonsieurMeh <mensoirmeh@gmail.com>
2026-03-13 20:41:39 -0700

    fixing method signature on flaregunrounditem_inflictdamageblock to match method correctly. Fixed projectiles sticking when we dont want them to. Added a toggle because its funi.

--- Main.cs
+++ Main.cs
@@ -2,6 +2,8 @@
 using MelonLoader;
 using HarmonyLib;
 using System.Reflection;
+using Il2CppInterop.Runtime.Injection;
+using UnityEngine.AddressableAssets.ResourceLocators;
 
 [assembly: AssemblyTitle("NonLethalFlares")]
 [assembly: AssemblyCopyright("MonsiuerMeh")]
@@ -12,26 +14,30 @@ using System.Reflection;
 
 namespace NonLethalFlares;
 
-internal class Main : MelonMod 
+internal class Main : MelonMod
 {
     internal static bool FlaregunDamageBlock = false;
+
+    public override void OnInitializeMelon()
+    {
+        Settings.Instance = new Settings();
+    }
 
     [HarmonyPatch(typeof(FlareGunRoundItem), nameof(FlareGunRoundItem.InflictDamage), new Type[] { typeof(UnityEngine.GameObject), typeof(float), typeof(bool), typeof(string) })]
     internal static class FlareGunRoundItem_InflictDamageBlock
     {
-        internal static void Prefix(UnityEngine.GameObject victim, float damageScalar, ref bool stickIn, string collider)
+        internal static void Prefix(UnityEngine.GameObject victim, float damageScalar, bool stickIn, string collider)
         {
             FlaregunDamageBlock = true;
-            stickIn = false;
+            stickIn = Settings.Instance.StickThemFlaresOnThemBears;
         }
 
-
-        internal static void Postfix() => FlaregunDamageBlock = false;
-    }
+        internal static void Postfix(UnityEngine.GameObject victim, float damageScalar, bool stickIn, string collider) => FlaregunDamageBlock = false;
+}
 
 
     [HarmonyPatch(typeof(BaseAi), nameof(BaseAi.ApplyDamage), new Type[] { typeof(float), typeof(float), typeof(DamageSource), typeof(string) })]
-    internal static class BaseAi_LogDamage
+    internal static class BaseAi_ApplyDamageBlock
     {
         internal static bool Prefix(float damage, float bleedOutMintues, DamageSource damageSource, string collider) => !FlaregunDamageBlock;
     }
---
+++ Settings.cs
@@ -0,0 +1,27 @@
+﻿using ModSettings;
+using Il2Cpp;
+
+
+namespace NonLethalFlares;
+
+internal class Settings : JsonModSettings
+{
+    internal static Settings Instance;
+
+    [Name("Enable Sticky Flare Rounds")]
+    [Description("Enable or disable flare gun rounds sticking to bears on impact. Its pretty goofy, but defaults to disabled.")]
+    public bool StickThemFlaresOnThemBears = false;
+
+
+    internal Settings() : base("NonLethalFlares")
+    {
+        Initialize();
+    }
+
+    protected void Initialize()
+    {
+        Instance = this;
+        AddToModSettings("Non-Lethal Flares");
+        RefreshGUI();
+    }
+}
